f9eb85aafe9dea06715c13dbdb5799ab7e17d166
William Torre <pro.williamtoro@gmail.com>
2024-07-15 18:54:51 -0500

    add background color on the screen and windows

--- src/navigation/AppNavigatior.styles.ts
+++ src/navigation/AppNavigatior.styles.ts
@@ -1,10 +1,12 @@
 import { StyleSheet } from 'react-native';
 import Constants from 'expo-constants';
+import theme from '../theme/theme';
 
 const styleAppNavigatior = StyleSheet.create({
   container : {
     marginTop: Constants.statusBarHeight,
-    flexGrow: 1
+    flexGrow: 1,
+    backgroundColor: theme.light.primary,
   }
 })
 export default styleAppNavigatior;

cc81cd601c89b28436c8b635c5291a8b925b50a9
William Torre <pro.williamtoro@gmail.com>
2024-07-19 21:32:08 -0500

    utils file import problem

--- src/components/InputDate/InputDate.tsx
+++ src/components/InputDate/InputDate.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import { View, Platform, Text, TouchableOpacity } from 'react-native';
 import DateTimePickerModal from "react-native-modal-datetime-picker";
-import { FormatingDate } from '../../utils/formatingDate';
+import { FormatingDate } from '../../utils/FormatingDate';
 
 // Type and Styles.
 import styles from './InputDate.style';

1b15358513de713bdadd9a23ae302391ab665c0a
William Torre <pro.williamtoro@gmail.com>
2024-07-25 13:18:29 -0500

    Types are removed from the ListFinancialProduct component because the props are passed directly form the context created for products, The data is extracted form the context and a unique key is placed in the FlatList iterator

--- src/components/ListFinancialProduct/ListFinancialProduct.tsx
+++ src/components/ListFinancialProduct/ListFinancialProduct.tsx
@@ -1,24 +1,30 @@
 import React from 'react';
-import { FlatList, View } from 'react-native';
+import { FlatList, View} from 'react-native';
+
+// Context
+import { useMyContextProduct } from '../../context/ProductContext';
 
 // Components
 import ItemFinancialProduct from '../CardItemProduct/ItemFinancialProduct';
 
 // Styles and Types
 import style from './ListFinancialProduct.styles';
-import { FinanceProductsProps } from '../../types/components/ListFinancialProduct.types';
 
+const ListFinancialProduct: React.FC = () => {
 
-const ListFinancialProduct: React.FC<FinanceProductsProps> = ({ data }) => {
+  const { products } = useMyContextProduct();
+
   return(
+    products &&
     <FlatList 
-      style={style.containerList}
-      data={data}
-      ItemSeparatorComponent={() => <View style={style.separator}/>}
-      renderItem={({ item: product}) => 
-        <ItemFinancialProduct id={product.id} name={product.name}/>
-      }
-    />
+        style={style.containerList}
+        data={products}
+        ItemSeparatorComponent={() => <View style={style.separator}/>}
+        keyExtractor={(product) => product.id.toString()}
+        renderItem={({ item: product}) => 
+          <ItemFinancialProduct id={product.id} name={product.name}/>
+        }
+      />
   )
 }
 

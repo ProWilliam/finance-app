5f0c3dd969cdc7d812fe34b38387a1b2dddd65ba
William Torre <pro.williamtoro@gmail.com>
2024-07-25 13:39:01 -0500

    useApi is used to bring us the information of the product found by its id in the search filter using the refetch and the new data. The value formatted as a single string found on the searched value is passed to the contet through setProduct to automatically update the data

--- src/components/SearchInput/SearchInput.tsx
+++ src/components/SearchInput/SearchInput.tsx
@@ -1,28 +1,50 @@
 import React, { useState } from 'react';
-import { Alert, TextInput, View } from 'react-native';
+import { TextInput, View } from 'react-native';
+
+// Hook
+import useApi from '../../hooks/useApi';
 
 // Component
 import SearchInputProps from '../../types/components/SearchInput.types';
 
-// Styles
+// Styles and Config
 import style from './SearchInput.styles';
+import config from '../../../app.config';
+import { useMyContextProduct } from '../../context/ProductContext';
 
 const SearchInput: React.FC<SearchInputProps> = ({ placeholder}) => {
-  const [searchText, setSearchText] = useState<string>('');
 
-  const handleSearch = () => {
-    Alert.alert('Buscando...');
+  const [searchText, setSearchText] = useState<string>('');
+  const { setProducts } = useMyContextProduct();
+  
+  const url = `${config.extra.productUrl + searchText}`
+  const { data, refetch } = useApi(url);
+
+  const handleSearch = async () => {
+
+    await refetch();
+
+    if (searchText === ''){
+      setProducts(data.data)
+    } else {
+      setProducts([{
+        id: data.id,
+        name: data.name
+      }])
+    }
   }
 
   return (
     <View style={style.containerSearch}>
-      <TextInput 
+      <TextInput
+        keyboardType='number-pad' 
         style={style.input}
         placeholder={placeholder}
         placeholderTextColor='black'
         value={searchText}
-        onChangeText={setSearchText}
+        onChangeText={(text) => setSearchText(text)}
         onSubmitEditing={handleSearch}
+        returnKeyType='done'
       />
     </View>
   )

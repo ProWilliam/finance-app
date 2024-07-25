c42dc1766da7cb96ab69f61b9ec29dd53bfd8903
William Torre <pro.williamtoro@gmail.com>
2024-07-25 12:23:18 -0500

    debug removal

--- src/utils/FormatingDate.ts
+++ src/utils/FormatingDate.ts
@@ -1,12 +1,9 @@
 export const FormatingDate = (dateString: any): string => {
 
-  console.log({dateString});
   // const date = new Date(dateString);
   const year = dateString.getFullYear();
   const month = String(dateString.getMonth() + 1).padStart(2, '0');
   const day = String(dateString.getDate()).padStart(2, '0');
-
-  console.log(`after: ${year}-${month}-${day}`);
 
   return `${year}-${month}-${day}`;
   

479187cbe644c693f6165088ec90854f05b3e095
William Torre <pro.williamtoro@gmail.com>
2024-08-17 23:47:01 -0500

    Load is added while the data is called from the database, in addtion the data is formatted so that it generically covers the data such as the card image and additional data

--- src/screens/InfoProduct/InfoProductScreen.tsx
+++ src/screens/InfoProduct/InfoProductScreen.tsx
@@ -1,26 +1,52 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, Text } from 'react-native';
+import { useRoute } from '@react-navigation/native';
 
 // Components
+import useApi from '../../hooks/useApi';
 import CardInfo from '../../components/CardInfo/CardInfo';
 import ButtonSubmit from '../../components/Button/ButtonSubmit';
 
 // Styles and config
 import styleInfoProduct from './InfoProductScreen.styles';
-import data from '../../data/allDataGet';
+// import data from '../../data/allDataGet';
+import config from '../../../app.config';
 
 
 const InfoProductScreen: React.FC = () => {
 
-  const props = data;
+  const routePath = useRoute();
+  const {id} = routePath.params as {id: string}
+
+  const { data, loading } = useApi(config.extra.productUrl + id);
+
+  const newData = {
+    ...data,
+    logo: config.extra.logo,
+    info_text: config.extra.rootInfo
+  }
+  
+  const props = newData;
   
   return (
-    <View style={styleInfoProduct.container} >
-      <CardInfo {...props} />
-      <View style={styleInfoProduct.buttonSection}>
-        <ButtonSubmit title='Editar' color='edit'/>
-        <ButtonSubmit title='Eliminar' color='deleted'/>
-      </View>
+    <View>
+      {
+        loading
+        ? 
+        <View>
+          <Text>
+            loading...
+          </Text>
+        </View>
+        : 
+        <View style={styleInfoProduct.container} >
+          <CardInfo {...props} />
+          <View style={styleInfoProduct.buttonSection}>
+            <ButtonSubmit title='Editar' color='edit'/>
+            <ButtonSubmit title='Eliminar' color='deleted'/>
+          </View>
+        </View>
+      }
     </View>
   )
 }

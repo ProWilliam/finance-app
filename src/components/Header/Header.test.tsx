b3b9498c334184461a29a6e22959fd41da8253af
William Torre <pro.williamtoro@gmail.com>
2024-09-09 12:19:39 -0500

    correct rendering, confirm that the title is displayed on the screen, also that the icon is there and the styles are applied correctly

--- src/components/Header/Header.test.tsx
+++ src/components/Header/Header.test.tsx
@@ -1,18 +1,35 @@
 import React from 'react';
+import { Text, View } from 'react-native';
 import renderer from 'react-test-renderer';
+
+// Component adn Styles
+import IconBank from '../../assets/icons/IconBank';
 import Header from './Header';
+import styles from './Header.styles';
 
 describe('<Header />', () => {
   it('renders correctly', () => {
-    const component = renderer.create(<Header />);
-    let tree = component.toJSON();
+    const tree = renderer.create(<Header />).toJSON();
+    
     expect(tree).toMatchSnapshot();
   });
 
   it('renders the title correctly', () => {
-    const component = renderer.create(<Header />);
-    const textComponent = component.root.findByType('Text');
+    const tree = renderer.create(<Header />);
+    const textComponent = tree.root.findByType(Text);
     expect(textComponent.props.children).toBe('Banco');
+  });
+
+  it('should render the IconBank component', () => {
+    const tree = renderer.create(<Header />);
+    const iconBank = tree.root.findByType(IconBank);
+    expect(iconBank).toBeTruthy();
+  });
+
+  it('should apply the correct styles to the container', () => {
+    const tree = renderer.create(<Header />);
+    const containerStyle = tree.root.findByType(View).props.style;
+    expect(containerStyle).toEqual(styles.container);
   });
 
 });

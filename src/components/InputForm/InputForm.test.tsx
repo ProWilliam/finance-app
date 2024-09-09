017949d988d143e899a77dec6c54dd01cc6dc96a
William Torre <pro.williamtoro@gmail.com>
2024-09-09 16:36:08 -0500

    Correct display of text and value to be reused in different formats, test for state change for the text according to information provided by the user, show error if it is rendered when coloring wrong data, check the value if it is numerical for the ID because should be placed for the backend index, It is verified that the input date behaves appropriately

--- src/components/InputForm/InputForm.test.tsx
+++ src/components/InputForm/InputForm.test.tsx
@@ -1,42 +1,138 @@
 import React from 'react';
 import renderer, { act } from 'react-test-renderer';
+import { TextInput } from 'react-native';
+
+// Components
+import InputDate from '../InputDate/InputDate';
 import InputForm from './InputForm';
+import styles from './InputForm.styles';
+
 
 describe('<InputForm />', () => {
+
+  let onChangeMock: jest.Mock;
+
+  beforeEach(() => {
+    onChangeMock = jest.fn();
+  });
+
   it('renders correctly with TextInput', () => {
-    const onChangeMock = jest.fn();
-    const component = renderer.create(
+    const tree = renderer.create(
       <InputForm
-        placeHolder="Enter text"
-        keySelect="inputKey"
+        placeHolder="Numero de ID"
+        keySelect="id"
         errorMessage="Campo obligatorio"
         onChange={onChangeMock}
-        type="text"
-        value=""
+        type="numeric"
+        value="1"
       />
-    );
-    let tree = component.toJSON();
+    ).toJSON();
+    
     expect(tree).toMatchSnapshot();
   });
 
-  it('calls onChange handler with correct value on text change', () => {
-    const onChangeMock = jest.fn();
-    const component = renderer.create(
+  it('should render TextInput and handle text changes', () => {
+
+    const tree = renderer.create(
       <InputForm
-        placeHolder="Enter text"
-        keySelect="inputKey"
-        errorMessage="Campo obligatorio"
+        placeHolder="Tarjeta Crédito"
+        keySelect="name"
+        errorMessage=""
         onChange={onChangeMock}
-        type="text"
+        type="default"
         value=""
       />
     );
 
+    const input = tree.root.findByType(TextInput);
+    expect(input.props.placeholder).toBe('Tarjeta Crédito');
+    
+    // Simulate text change
     act(() => {
-      component.root.findByType('TextInput').props.onChangeText('Test input');
+      input.props.onChangeText('New text');
     });
 
-    expect(onChangeMock).toHaveBeenCalledWith('inputKey', 'Test input');
+    // Check if onChange is called with the correct parameters
+    expect(onChangeMock).toHaveBeenCalledWith('name', 'New text');
+    
+    // Verify that error is cleared
+    const errorText = tree.root.findByProps({ testID: 'error-message' });
+    expect(errorText.props.children).toBeUndefined();
   });
+  
+  it('should show error message when input is blurred with empty value', () => {
+    const tree  = renderer.create(
+      <InputForm
+        placeHolder="Descripción"
+        keySelect="description"
+        errorMessage=""
+        onChange={onChangeMock}
+        type="default"
+        value=""
+      />
+    );
+
+    const input = tree.root.findByType(TextInput);
+    
+    // Simulate blur event
+    act(() => {
+      input.props.onBlur();
+    });
+
+    // Verify error message is shown
+    const errorText = tree.root.findByProps({ style: styles.textError });
+    expect(errorText.props.children).toBe('Este campo es obligatorio!');
+  });
+
+  it('calls onChange handler with correct value on text change and date ID is numeric', () => {
+    const tree = renderer.create(
+      <InputForm
+        placeHolder="ID"
+        keySelect="id"
+        errorMessage="Campo obligatorio"
+        onChange={onChangeMock}
+        type="numeric"
+        value=""
+      />
+    );
+
+    // Find the TextInput component
+    const textInput = tree.root.findByType(TextInput);
+
+    act(() => {
+      textInput.props.onChangeText('1');
+    });
+
+    // Verify the Key and Value.
+    expect(onChangeMock).toHaveBeenCalledWith('id', '1');
+
+    // Verify the numeric conversion
+    const args = onChangeMock.mock.calls[0][1]; // Get the argument passed to onChangeMock
+    const numericValue = Number(args);
+
+    expect(typeof numericValue).toBe('number');
+    expect(isNaN(numericValue)).toBe(false); // Ensure it is not NaN
+
+  });
+
+  it('should render InputDate component for type="date"', () => {
+    const tree = renderer.create(
+      <InputForm
+        placeHolder="22/02/2023"
+        keySelect="date_release"
+        errorMessage=""
+        onChange={onChangeMock}
+        type="date"
+        value="2024-08-10"
+      />
+    );
+
+    // Verify InputDate component is rendered
+    const inputDate = tree.root.findByType(InputDate);
+    expect(inputDate.props.title).toBe('22/02/2023');
+    expect(inputDate.props.keyValue).toBe('date_release');
+    expect(inputDate.props.value).toBe('2024-08-10');
+  });
+
 
 });

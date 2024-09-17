6e81720c445156f5a51e46c91620264b2f63a814
William Torre <pro.williamtoro@gmail.com>
2024-09-16 21:21:17 -0500

    A View section is created with Body styles to create the full element width according to its platform and image width, DimencionsScreenPlataforms is used to return the allowed width according to its rendering. It is created according to the platform to create space in the return button in addition to adding a return button to the beginning

--- src/screens/RegisterForm/RegisterForm.styles.ts
+++ src/screens/RegisterForm/RegisterForm.styles.ts
@@ -1,20 +1,35 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Platform } from 'react-native';
 import theme from '../../theme/theme';
+import { DimecionsScreenPlataforms } from '../../utils/DimencionsScreenPlataform';
 
 const RegisterFormStyle =  StyleSheet.create({
-  containerRegister: {
-    width: `${theme.withScreen.big}%`,
-    flex:1,
-    paddingBottom: 20,
-    paddingLeft: 60,
-    backgroundColor: '#fefefe',
+  body: {
+    height: "100%",
+    alignItems: "center",
+    backgroundColor: theme.light.primary,
   },
-  scroll:{
-    width: `${theme.withScreen.big}%`,
+  containerRegister: {
+    flex: 1,
+    alignItems: 'center',
+    backgroundColor: theme.light.primary,
+    width: `${DimecionsScreenPlataforms()}%`,
+  },
+  sectionTitle: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  iconCard: {
+    marginLeft: Platform.OS === 'web' ? 200 : 0,
+    color: theme.light.tertiary
   },
   title: {
     fontSize: theme.size.fontMedium
-  }
+  }, 
+  scroll:{
+    marginLeft: '20%',
+    width: `${theme.withScreen.big}%`,
+  },
 })
 
 export default RegisterFormStyle;
--- src/screens/RegisterForm/RegisterFormScreen.tsx
+++ src/screens/RegisterForm/RegisterFormScreen.tsx
@@ -1,11 +1,13 @@
-import React from 'react';
-import {View, Text, ScrollView, Alert } from 'react-native';
+import React, { useState } from 'react';
+import {View, Text, ScrollView, Alert, TouchableOpacity } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 
 // Hook and components
 import useForm from '../../hooks/useForm';
+import useAppNavigation from '../../hooks/useAppNavigation';
 import AddProduct from '../../components/AddProduct/AddProduct';
 import ButtonSubmit from '../../components/Button/ButtonSubmit';
+import IconClose from '../../assets/icons/IconClose';
 
 // Styles
 import style from './RegisterForm.styles';
@@ -13,8 +15,17 @@ import style from './RegisterForm.styles';
 
 const RegisterFormScreen: React.FC = () => {
 
+  const [route, setRoute] = useState('');
+  const { appNavigation } = useAppNavigation();
+
+  appNavigation(route)
+
   const { formData, handleInputChange, handleSubmit, error, resetForm } = useForm();
   const navigation = useNavigation();
+
+  const goBack = () => {
+    setRoute('home');
+  }
 
   const submit = async () => {
 
@@ -30,20 +41,29 @@ const RegisterFormScreen: React.FC = () => {
   }
 
   return(
-    <View style={style.containerRegister}>
-      <Text style={style.title}>
-        Registro de Productos
-      </Text>
-      <ScrollView style={style.scroll}>
-        <AddProduct title='ID' placeHolder='Numero de ID' keySelect={'id'} onChange={handleInputChange} type='numeric' value={formData.id} />
-        <AddProduct title='Nombre' placeHolder='Tarjeta Crédito' keySelect={'name'} onChange={handleInputChange} type='default' value={formData.name}/>
-        <AddProduct title='Descripción' placeHolder='Descripción' keySelect={'description'} onChange={handleInputChange} type='default' value={formData.description}/>
-        <AddProduct title='Logo' placeHolder='Logo'  keySelect={'logo'} onChange={handleInputChange} type='default' value={formData.logo}/>
-        <AddProduct title='Fecha Liberación' placeHolder='22/02/2023'  keySelect={'date_release'} onChange={handleInputChange} type='date' value={formData.date_release}/>
-        <AddProduct title='Fecha Revisión' placeHolder='22/02/2024'  keySelect={'date_revision'} onChange={handleInputChange} type='date' value={formData.date_revision}/>
-        <ButtonSubmit title='Enviar' color='send' press={submit} />
-        <ButtonSubmit title='Reiniciar' color='edit' press={resetForm} />
-      </ScrollView>
+    <View style={style.body}>
+      <View style={style.containerRegister}>
+        <View style={style.sectionTitle}>
+          <Text style={style.title}>
+            Registro de Productos
+          </Text>
+          <TouchableOpacity
+            onPress={goBack}
+          >
+            <IconClose style={style.iconCard}/>
+          </TouchableOpacity>
+        </View>
+        <ScrollView style={style.scroll}>
+          <AddProduct title='ID' placeHolder='Numero de ID' keySelect={'id'} onChange={handleInputChange} type='numeric' value={formData.id} />
+          <AddProduct title='Nombre' placeHolder='Tarjeta Crédito' keySelect={'name'} onChange={handleInputChange} type='default' value={formData.name}/>
+          <AddProduct title='Descripción' placeHolder='Descripción' keySelect={'description'} onChange={handleInputChange} type='default' value={formData.description}/>
+          <AddProduct title='Logo' placeHolder='Logo'  keySelect={'logo'} onChange={handleInputChange} type='default' value={formData.logo}/>
+          <AddProduct title='Fecha Liberación' placeHolder='22/02/2023'  keySelect={'date_release'} onChange={handleInputChange} type='date' value={formData.date_release}/>
+          <AddProduct title='Fecha Revisión' placeHolder='22/02/2024'  keySelect={'date_revision'} onChange={handleInputChange} type='date' value={formData.date_revision}/>
+          <ButtonSubmit title='Enviar' color='send' press={submit} />
+          <ButtonSubmit title='Reiniciar' color='edit' press={resetForm} />
+        </ScrollView>
+      </View>
     </View>
   );
 }
